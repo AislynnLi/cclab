bf992a6385b11a8db30ebdd1d4bfb6c90c7b6c24
Aislynn <ql2103@nyu.edu>
2021-11-12 15:31:38 +0800

    new p5js

--- js-and-dom/js/script.js
+++ js-and-dom/js/script.js
@@ -1,3 +1,43 @@
+let angle = 0;     // the starting angle
+let radius = 100;  // distance to center
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  let centerX = width/2;  // rotating around the
+  let centerY = height/2; // center of the screen
+
+  // we use sin() and cos() to get how much
+  // the ellipse is to the right (dx) and bottom
+  // (dy) of the center based on the angle and
+  // the radius you want
+  let dx = cos(radians(angle)) * radius;
+  let dy = sin(radians(angle)) * radius;
+
+  // put center and offset together to get
+  // screen coordinates
+  let x = centerX + dx;
+  let y = centerY + dy;
+
+  // voila!
+  console.log('ellipse is at ' + x + ', ' + y);
+  ellipse(x, y, 10, 10);
+
+  // here you can decide how fast your circle
+  // should rotate
+  angle++;
+}
+
+
+
+
+
+
+/*
 let roundButtom = document.getElementById('roundButtom');
 roundButtom.addEventListener('click',roundButtomClicked);
 
@@ -34,3 +74,4 @@ function say(what) {
 }
 
   }
+  */
